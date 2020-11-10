47a0f735ec6a2616bec58ad7ea946ce23a674945
Mladen Ilic <mladenilic480@gmail.com>
2020-11-10 16:41:28 +0100

    Setup basic project structure

--- cli.js
+++ cli.js
@@ -4,14 +4,15 @@ const React = require('react');
 const {render} = require('ink');
 const meow = require('meow');
 
-const ui = require('import-jsx')('./ui');
+const app = require('import-jsx')('./src/app');
 
 const cli = meow(`
   Usage
     $ arduino-cereal
 
   Options
+    --port  Serial port
     --baud  Baud rate (default: 9600)
 `);
 
-render(React.createElement(ui, cli.flags));
+render(React.createElement(app, cli.flags));
