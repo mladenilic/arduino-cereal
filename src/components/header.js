315740d2b894ac84e7bc07d93ea42bc37cd83e5b
Mladen Ilic <mladenilic480@gmail.com>
2020-11-12 00:52:33 +0100

    Add serial port status to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 'use strict';
+
 const React = require('react');
+const { Spacer } = require('ink');
 
 const Box = require('import-jsx')('./base/box');
 const Text = require('import-jsx')('./base/text');
@@ -7,13 +9,24 @@ const Heading = require('import-jsx')('./base/heading');
 
 const { connect } = require('react-redux/lib/alternate-renderers');
 
-const Header = ({ config }) => {
+const Header = ({ config, serial }) => {
+  const status = () => {
+    switch (serial.status) {
+      case 'success': return <Text color="#0f0">connected</Text>;
+      case 'error': return <Text color="#f00">error</Text>;
+      case 'pending': return <Text color="#00f">connecting...</Text>;
+      default: return null;
+    }
+  };
+
   return <Box paddingX={1} paddingY={0} borderStyle="single">
     <Heading>Arduino Cereal Monitor</Heading>
     <Text> — Port: <Text bold>{config.port || '/'}</Text>, Baud rate: <Text bold>{config.baud}</Text></Text>
+    <Spacer/>
+    <Text>Status: {status()}</Text>
   </Box>
 };
 
 module.exports = connect(
-  (state) => ({ config: state.config }),
+  (state) => ({ config: state.config, serial: state.serial }),
 )(Header);
