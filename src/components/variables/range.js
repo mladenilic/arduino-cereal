f0a9c5f16e20a3da8517c341be314957293d304e
Mladen Ilic <mladenilic480@gmail.com>
2020-11-18 01:35:13 +0100

    Avoid variable renders

--- src/components/variables/range.js
+++ src/components/variables/range.js
@@ -6,15 +6,15 @@ import VariableName from './base/name';
 import VariableValue from './base/value';
 import Gauge from './base/gauge';
 
-const Range = ({ name, variable }) => {
+const Range = React.memo(({ variable }) => {
   const [min, max] = variable.options.map(a => parseFloat(a));
   const value = parseFloat(variable.value);
 
   return <Box flexDirection="row">
-    <VariableName name={name} />
+    <VariableName name={variable.name} />
     <Gauge min={min} max={max} value={value}/>
     <Text> (<VariableValue value={value}/>)</Text>
   </Box>
-};
+});
 
 export default Range;
--- src/components/variables/variable.js
+++ src/components/variables/variable.js
@@ -4,11 +4,11 @@ import Box from '../base/box';
 import VariableName from './base/name';
 import VariableValue from './base/value';
 
-const Variable = ({ name, variable }) => {
-  return <Box flexDirection="row">
-    <VariableName name={name}/>
+const Variable = React.memo(({ variable }) => (
+  <Box flexDirection="row">
+    <VariableName name={variable.name}/>
     <VariableValue value={variable.value}/>
   </Box>
-};
+));
 
 export default Variable;
