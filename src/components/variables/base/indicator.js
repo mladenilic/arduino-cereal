f440551deac9e509e7512c3d29c7690fbf94311b
Mladen Ilic <mladenilic480@gmail.com>
2020-11-19 01:43:36 +0100

    Create flag indicator component

---
+++ src/components/variables/base/indicator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Text from '../../base/text';
+import { connect } from 'react-redux/lib/alternate-renderers';
+
+const Indicator = ({ value, colors, characters }) => (
+  <Text>[<Text color={value ? colors.on : colors.off}>{value ? characters.on : characters.off}</Text>] </Text>
+);
+
+export default connect(
+  (state) => ({
+    characters: state.config?.theme?.modules?.variables?.flag || {},
+    colors: state.config?.theme?.colors?.variables?.flag || {}
+  }),
+)(Indicator);
+
--- src/components/variables/flag.js
+++ src/components/variables/flag.js
@@ -1,29 +1,18 @@
 import React from 'react';
 
 import Box from '../base/box';
-import Text from '../base/text';
+import Indicator from './base/indicator';
 import VariableName from './base/name';
 import VariableValue from './base/value';
 
-import { connect } from 'react-redux/lib/alternate-renderers';
-
-const Flag = ({ variable, characters, colors }) => {
-  const value = parseInt(variable.value);
+const Flag = ({ variable }) => {
+  const value = !!parseInt(variable.value);
 
   return <Box flexDirection="row">
     <VariableName name={variable.name} />
-    <Text>
-      { value ?
-        <Text>[<Text color={colors.on}>{(characters.on || '▣')}</Text>] <VariableValue value="HIGH"/></Text> :
-        <Text>[<Text color={colors.on}>{(characters.off || '▢')}</Text>] <VariableValue value="LOW"/></Text>
-      }
-    </Text>
+    <Indicator value={value} />
+    <VariableValue value={value ? 'HIGH' : 'LOW'} />
   </Box>
 };
 
-export default connect(
-  (state) => ({
-    characters: state.config?.theme?.modules?.variables?.flag || {},
-    colors: state.config?.theme?.colors?.variables?.flag || {},
-  }),
-)(Flag);
+export default Flag;
