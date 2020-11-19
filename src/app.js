61c333f9d7a1f8c18e7fdc718109efc966b0d9c6
Mladen Ilic <mladenilic480@gmail.com>
2020-11-19 18:11:01 +0100

    Show error if no port is provided

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import { setSerialStatus } from './redux/actions/serial';
 import Serial from './serial';
 
 import Box from './components/base/box';
+import Text from './components/base/Text';
 import Header from './components/header';
 import Variables from './components/variables';
 import Monitor from './components/monitor';
@@ -23,12 +24,20 @@ const App = ({ config, setConfig, updateVariable, outputVariables, addMessage, o
   }, 1000 / Math.min(Math.max(config.fps || 15, 1), 60)));
 
   useInit(() => {
+    if (!config.port) {
+      return;
+    }
+
     Serial.connect(config.port, config.baud || 9600)
       .on('connect', () => setSerialStatus('success'))
       .on('error', () => setSerialStatus('error'))
       .on('variable', ([type, name, value, ...options]) => updateVariable(type, name, value, options))
       .on('message', message => addMessage(message));
   });
+
+  if (!config.port) {
+    return <Text>ERROR: Please specify serial port.</Text>;
+  }
 
   return (
     <Box flexDirection="column" height={process.stdout.rows - 1}>
